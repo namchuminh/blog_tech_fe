4faf9326919827296ce3aba6435224ef9226a07f
Nam Chu Minh <chuminhnamma@gmail.com>
2024-10-16 19:15:59 +0700

    xu ly truong hop user admin dang nhap 1 trinh duyet:

--- src/layouts/Layout.js
+++ src/layouts/Layout.js
@@ -1,11 +1,24 @@
 import React, { useEffect, lazy, Suspense } from 'react';
 import { useLocation } from 'react-router-dom';
 import AppRoutes from '../routes';
+import CryptoJS from 'crypto-js';
 
 const Header = lazy(() => import('./Header'));
 const Footer = lazy(() => import('./Footer'));
 const UserHeader = lazy(() => import('./User/Header'));
 const UserFooter = lazy(() => import('./User/Footer'));
+
+
+const decodeJWT = (token) => {
+  const parts = token.split('.');
+  if (parts.length !== 3) {
+      throw new Error('JWT không hợp lệ');
+  }
+  
+  const payload = parts[1];
+  const decoded = CryptoJS.enc.Base64.parse(payload);
+  return JSON.parse(decoded.toString(CryptoJS.enc.Utf8));
+};
 
 const Layout = () => {
   const location = useLocation();
@@ -23,8 +36,22 @@ const Layout = () => {
   useEffect(() => {
     if (isAdmin) {
       import('../assets/admin.css');
+      if (localStorage.getItem('token')) {
+        const decoded = decodeJWT(localStorage.getItem('token'));
+        if(decoded.role == 'user'){
+          localStorage.removeItem('token')
+          localStorage.removeItem('refreshToken')
+        }
+      }
     } else {
       import('../assets/user.css');
+      if (localStorage.getItem('token')) {
+        const decoded = decodeJWT(localStorage.getItem('token'));
+        if(decoded.role == 'admin'){
+          localStorage.removeItem('token')
+          localStorage.removeItem('refreshToken')
+        }
+      }
     }
   }, [isAdmin]);
 
